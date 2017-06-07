3159ba20edcfa5b5f0671cd674b7a2dc563cf1ba
Adrien Guillo <adrien.guillo@gmail.com>
2017-06-07 14:42:25 -0400

    Exercise 4.6: implement `map`, `flatMap`, `orElse` and `map2`

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -3,19 +3,40 @@ package fpinscala.errorhandling
 
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
+
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def isLeft: Boolean = this match {
+    case Left(_) => true
+    case _ => false
+  }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def isRight: Boolean = !isLeft
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+ def map[B](f: A => B): Either[E, B] = this match {
+   case left @ Left(_) => left
+   case Right(value) => Right(f(value))
+ }
+
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+   case left @ Left(_) => left
+   case Right(value) => f(value)
+ }
+
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+   if (isRight) this else b
+
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+   b.flatMap { bb => map { aa => f(aa, bb) } } // or for-comprehension
+
 }
+
 case class Left[+E](get: E) extends Either[E,Nothing]
+
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
+
   def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
 
   def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
