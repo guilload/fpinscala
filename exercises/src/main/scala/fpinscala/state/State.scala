c529c95694c45f15d19943ef988b34734058e8b3
Adrien Guillo <adrien.guillo@gmail.com>
2017-06-13 17:41:38 -0400

    Exercise 6.9: implement `mapViaFlatMap` and `map2ViaFlatMap`

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -108,6 +108,12 @@ object RNG {
       else nonNegativeLessThan(n)
     }
 
+  def mapViaFlatMap[A, B](rand: Rand[A])(f: A => B): Rand[B] =
+    flatMap(rand)(a => unit(f(a)))
+
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+
 }
 
 case class State[S,+A](run: S => (A, S)) {
