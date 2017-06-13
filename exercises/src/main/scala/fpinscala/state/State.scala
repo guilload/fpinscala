076c8174e54c400b2cd9e9c14e857eb8fc01e685
Adrien Guillo <adrien.guillo@gmail.com>
2017-06-13 18:35:14 -0400

    Exercise 6.10: implement `State` functions
    `map`, `map2`, `flatMap`, `unit`, and `sequence`

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -113,16 +113,42 @@ object RNG {
 
   def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
     flatMap(ra)(a => map(rb)(b => f(a, b)))
+  }
 
 }
 
-case class State[S,+A](run: S => (A, S)) {
+
+case class State[S, +A](run: S => (A, S)) {
+
   def map[B](f: A => B): State[S, B] =
-    ???
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
+    flatMap(a => State.unit(f(a)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
+
   def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+    State(
+      s => {
+        val (a, next) = run(s)
+        f(a).run(next)
+      }
+    )
+
+}
+
+
+object State {
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  def sequence[S, A](xs: List[State[S, A]]): State[S, List[A]] =
+    xs.foldRight(unit[S, List[A]](List()))((s, acc) => s.map2(acc)(_ :: _))
+
+  type Rand[A] = State[RNG, A]
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+
 }
 
 sealed trait Input
@@ -130,8 +156,3 @@ case object Coin extends Input
 case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
-
-object State {
-  type Rand[A] = State[RNG, A]
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
-}
