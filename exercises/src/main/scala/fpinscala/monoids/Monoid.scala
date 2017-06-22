ccf9ef6c9a9cf830bfbfa2ce1a526ec72b421435
Adrien Guillo <adrien.guillo@gmail.com>
2017-06-22 17:19:10 -0400

    Exercise 10.1: define monoids `intAddition`, `intMultiplication`, ...
    `booleanAnd`, and `booleanOr`

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -21,13 +21,25 @@ object Monoid {
     val zero = Nil
   }
 
-  val intAddition: Monoid[Int] = ???
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 + a2
+    val zero: Int = 0
+  }
 
-  val intMultiplication: Monoid[Int] = ???
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 * a2
+    val zero: Int = 1
+  }
 
-  val booleanOr: Monoid[Boolean] = ???
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+    val zero: Boolean = true
+  }
 
-  val booleanAnd: Monoid[Boolean] = ???
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+    val zero: Boolean = false
+  }
 
   def optionMonoid[A]: Monoid[Option[A]] = ???
 
