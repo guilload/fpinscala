c884b89f63e2466f7119e7e0eb9fd7c1bab33973
Adrien Guillo <adrien.guillo@gmail.com>
2017-06-29 16:00:41 -0400

    Exercise 11.20: implement `readerMonad`

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -139,13 +139,17 @@ case class Id[A](value: A) {
 
 }
 
+case class Reader[R, A](run: R => A)
+
 object Reader {
 
   def readerMonad[R] = new Monad[({type f[x] = Reader[R, x]})#f] {
 
-    def unit[A](a: => A): Reader[R, A] = ???
+    def unit[A](a: => A): Reader[R, A] = Reader((_:R) => a)
 
-    override def flatMap[A, B](st: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] = ???
+    def flatMap[A, B](st: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] =
+      Reader(r => f(st.run(r)).run(r))
+
   }
 
 }
