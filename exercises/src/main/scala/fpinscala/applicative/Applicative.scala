b72e43179f972b3e0b9dfea2327d455523a5ad59
Adrien Guillo <adrien.guillo@gmail.com>
2017-07-08 15:33:28 -0400

    Exercise 12.12: implement `sequenceMap`

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -69,7 +69,8 @@ trait Applicative[F[_]] extends Functor[F] {
 
   }
 
-  def sequenceMap[K, V](ofa: Map[K, F[V]]): F[Map[K, V]] = ???
+  def sequenceMap[K, V](ofa: Map[K, F[V]]): F[Map[K, V]] =
+    ofa.foldRight(unit(Map.empty[K, V])) { case ((key, fvalue), acc) => map2(acc, fvalue)((m, value) => m + (key -> value)) }
 }
 
 case class Tree[+A](head: A, tail: List[Tree[A]])
