85a77980357cfb07ad031d02fc26d8e8922db46f
Adrien Guillo <adrien.guillo@gmail.com>
2017-07-29 17:29:24 -0400

    Exercise 15.06: implement `zipWithIndex`

--- exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -216,7 +216,11 @@ object SimpleStreamTransducers {
     /*
      * Exercise 6: Implement `zipWithIndex`.
      */
-    def zipWithIndex: Process[I,(O,Int)] = ???
+    def zipWithIndex: Process[I, (O, Int)] = for {
+      value <- this
+      i <- count
+    }
+      yield (value, i - 1)
 
     /* Add `p` to the fallback branch of this process */
     def orElse(p: Process[I, O]): Process[I, O] = this match {
